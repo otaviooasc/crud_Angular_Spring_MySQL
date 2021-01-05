e5537fe5d5b2ecf9e9402c7172b27b559c21be1a
Otávio Cavalcanti <otaviooasc@gmail.com>
2021-01-05 15:17:04 -0300

    Fim do projeto

--- crudFront/src/app/product/detail-product.component.ts
+++ crudFront/src/app/product/detail-product.component.ts
@@ -1,3 +1,7 @@
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from './../service/-product.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Product } from './../models/product';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail-product.component.css']
 })
 export class DetailProductComponent implements OnInit {
+  product: Product = null;
 
-  constructor() { }
+  constructor(
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.params.id;
+    this.productService.detail(id).subscribe(
+      data =>{
+        this.product = data;
+      },
+      err => {
+        this.toastr.error(err.error.message, 'Fail', {
+          timeOut:3000, positionClass: 'toast-top-center',
+        });
+        this.onDetail();
+      }
+    );
+  }
+
+  onDetail(): void{
+    this.router.navigate(['/']);
   }
 }
